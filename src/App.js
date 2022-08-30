533efd437c810650db60709035092d30e6013c22
mehdi <mkoushki.79@gmail.com>
2022-08-30 08:31:55 -0700

    install react bootstrap and create pagintion

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Login from "./components/login/Login";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Header from "./components/home/header/Header";
+import './bootstarap/bootstrap.css'
 import "./App.css"
 import Content from "./components/home/content/Content";
 function App() {
--- src/components/home/content/Content.js
+++ src/components/home/content/Content.js
@@ -1,12 +1,25 @@
-import React,{useEffect} from 'react'
+import React,{useEffect,useState} from 'react'
 import styles from './content.module.css'
 import { useDispatch , useSelector} from 'react-redux'
 import { PostAction } from '../../redux/action'
+import {Pagination} from 'react-bootstrap';
 
 const Content = () => {
     const dispatch= useDispatch()
     const {posts}= useSelector(state=>state.getPosts)
-    console.log(posts);
+    const [page, setpage] = useState(1)
+    const [pagination, setpagination] = useState([])
+    const [paginationEnd, setpaginationEnd] = useState([])
+
+   useEffect(() => {
+    setpagination(
+        posts.filter((item,index)=>!(index % 12))  
+    )
+    setpaginationEnd(
+        posts.filter((item,index)=>!(index % 24))  
+    )
+   
+   }, [posts])
     
     useEffect(() => {
         dispatch(PostAction())
@@ -19,17 +32,50 @@ const Content = () => {
             <button>Serch</button>
         </div>
         <div className={styles.Post}>
-            {posts.map((item)=>{
+            {posts.slice((page -1) * 12 ,page * 12).map((item)=>{
+                
                 return(
                     <div key={item.id} className={styles.ContentItem}>
                         <h2>{item.title}</h2>
                         <p>{item.body}</p>
                         <button>edite post</button>
+                        <h3>{item.id}</h3>
 
                     </div>
                 )
             })}
+          
         </div>
+      
+ <Pagination>
+      <Pagination.First   onClick={()=>{
+        setpage(last=> last - 1)
+    }} />
+
+      {paginationEnd.map((item,index)=><Pagination.Item key={index} active={index + 1 === page}
+      onClick={()=>{
+        setpage(index + 1)
+      }}
+      >{index + 1}</Pagination.Item>
+
+      )}
+      <Pagination.Ellipsis />
+      <Pagination.Item 
+      onClick={()=>{
+        setpage(pagination.length)
+      }}
+      
+      active={page===pagination.length}>{pagination.length} </Pagination.Item>
+
+      
+      <Pagination.Last onClick={()=>{
+                setpage(last=>last+1)
+            }}
+    
+      />
+     
+    </Pagination>
+            
     </div>
   )
 }
