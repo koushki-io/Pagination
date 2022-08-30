3cc86d517a4aa851c547fb9393040b2b998cc2ff
mehdi <mkoushki.79@gmail.com>
2022-08-30 02:38:57 -0700

    add serch box in content

--- src/components/home/content/Content.js
+++ src/components/home/content/Content.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import styles from './content.module.css'
 
 const Content = () => {
   return (
-    <div  >Content</div>
+    <div className={styles.Content}>
+        <h2>Post</h2>
+        <div className={styles.SerchBox}>
+            <input type="text" />
+            <button>Serch</button>
+        </div>
+        <div className={styles.Post}></div>
+    </div>
   )
 }
 
